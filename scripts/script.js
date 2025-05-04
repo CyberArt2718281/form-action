a6d6e07ca560a660b9dfb20106d2f5659086e330
ArtemFrontStack <artem2006pax@mail.ru>
2025-05-04 03:53:36 +0300

    added string in script.js mytask

--- scripts/script.js
+++ scripts/script.js
@@ -151,4 +151,5 @@ window.onload = function () {  // функция выполняет код вн�
     });
 
     login.addEventListener('click', loginClick); // переключение на форму входа
+    console.log('final');
 }
